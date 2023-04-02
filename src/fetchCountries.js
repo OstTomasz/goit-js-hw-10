78d3eb03136b3e11fb258e82c097544cca4d4126
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-04-02 15:54:14 +0200

    filtercountry

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,45 +1,12 @@
-import debounce from 'lodash.debounce';
-
-const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-const DEBOUNCE_DELAY = 300;
-
-export const fetchCountries = fetch(
-  'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages'
-);
-// https://restcountries.com/v3.1/name/{name}
-
-let inputedCountry = '';
-searchBox.addEventListener(
-  'input',
-  debounce(() => {
-    inputedCountry = searchBox.value.trim();
-    console.log(inputedCountry);
-  }, DEBOUNCE_DELAY)
-);
-
-function listCountries(countries) {
-  let countriesListHTML = '';
-
-  countries.forEach(country => {
-    countriesListHTML += `<li>${country.name.official}</li>`;
-  });
-  countryList.innerHTML = countriesListHTML;
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
 }
-
-fetchCountries
-  .then(response => {
-    const countriesData = response.json();
-    return countriesData;
-  })
-  .then(countries => {
-    console.log(countries);
-    listCountries(countries);
-  })
-  .catch(error => {
-    console.log(`oops! We have a problem: ${error}`);
-  });
-
-//todo
-// filtercountry
--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-// https://restcountries.com/v3.1/name/{name}
+let input = '';
+searchBox.addEventListener(
+  'input',
+  debounce(() => {
+    input = searchBox.value.trim();
+    let countriesListHTML = '';
+    let countryName = '';
+    if (input) {
+      fetchCountries(input).then(countries => {
+        countries.forEach(country => {
+          countryName = country.name.official;
+          countriesListHTML += `<li>${countryName}</li>`;
+          countryList.innerHTML = countriesListHTML;
+        });
+      });
+    } else if (!input) {
+      countryList.innerHTML = '';
+    }
+  }, DEBOUNCE_DELAY)
+);
+
+//     countryName = inputedCountry.name;
+//     console.log(countryName);
+
+//todo
+// filtercountry
+// countries.forEach(country => {
+//   countryName = country.name.official;
+//   if (countryName.includes(inputedCountry)) {
+//     countriesListHTML += `<li>${countryName}</li>`;
+//   }
+//   countryList.innerHTML = countriesListHTML;
+// });
+// if (!inputedCountry) {
+
+// }
